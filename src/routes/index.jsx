2d5605624a8d4131fe5f77233d3161aa4d9dab77
MertMemis <mertmemis66@gmail.com>
2023-12-28 21:59:46 +0300

    Routes Created

---
+++ src/routes/index.jsx
@@ -0,0 +1,64 @@
+import { createBrowserRouter } from "react-router-dom";
+import Opening from "~/pages/opening";
+import Dubbing from "~/pages/browse/dubbing";
+import OriginalLanguage from "~/pages/browse/original-audio";
+import Subtitles from "~/pages/browse/subtitle";
+import Home from "~/pages/home";
+import Latest from "~/pages/latest";
+import Movies from "~/pages/movies";
+import MyList from "~/pages/my-list";
+import NotFound from "~/pages/not-found";
+import TvShows from "~/pages/tv-shows";
+import MainLayout from "~/layouts/main";
+
+
+
+export const routes = createBrowserRouter([
+   {
+      path: '/',
+      element: <MainLayout />,
+      children: [
+         {
+            index: true,
+            element: <Home />
+         },
+         {
+            path: '/tv-shows',
+            element: <TvShows />
+         },
+         {
+            path: '/movies',
+            element: <Movies />
+         },
+         {
+            path: '/latest',
+            element: <Latest />
+         },
+         {
+            path: '/my-list',
+            element: <MyList />
+         },
+         {
+            path: '/original-audio',
+            element: <OriginalLanguage />
+         },
+         {
+            path: '/audio',
+            element: <Dubbing />
+         },
+         {
+            path: '/subtitles',
+            element: <Subtitles />
+         }
+      ]
+   },
+   
+   {
+      path: '/opening',
+      element: <Opening />
+   }, 
+   {
+      path: '*',
+      element: <NotFound/>
+   }
+])
