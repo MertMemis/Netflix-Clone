ee630aa50173f60ec950bc4c8a70427369a8d5dc
MertMemis <mertmemis66@gmail.com>
2023-12-29 21:56:43 +0300

    imports

--- src/layouts/main/header/index.jsx
+++ src/layouts/main/header/index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import NetflixLogo from "~/assets/logo/netflix-logo";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { Link, NavLink } from "react-router-dom";
+import { headerMenu } from "~/utils/const";
+import classNames from "classnames";
+import SearchBar from "./search-bar";
 
 export default function Header() {
    return(
