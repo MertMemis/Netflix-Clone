45bb7d5750ea3e4aeb3848c83bc2f0da0af16b05
MertMemis <mertmemis66@gmail.com>
2023-12-29 22:05:51 +0300

    Header menu

--- src/layouts/main/header/index.jsx
+++ src/layouts/main/header/index.jsx
@@ -8,7 +8,25 @@ import SearchBar from "./search-bar";
 
 export default function Header() {
    return(
+      <header className="w-full bg-black min-h-[70px] flex sticky top-0">
+         <div className="flex items-center sticky mx-[60px] w-full">
             <NetflixLogo />
+            <nav className=" flex item-center">    
+               {headerMenu.map((menu, index) => (
+                  <NavLink to={menu.path} >
+                     {({isActive}) => (
+                        <div 
+                           type="button"
+                           className={classNames("hover:text-[#b3b3b3] transition-colors text-[14px] ml-5", {
+                              "font-bold cursor-default hover:text-[#e5e5e5]": isActive
+                           })}
+                        >
+                           {menu.title}
+                        </div>
+                     )}
+                  </NavLink>
+               ))}
+            </nav>
       </header>
    )
 }
