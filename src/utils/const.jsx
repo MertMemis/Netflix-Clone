ac35f2148449c643e8e1d0e2da1bae680b0c8d49
MertMemis <mertmemis66@gmail.com>
2023-12-29 21:59:11 +0300

    utils for menu url's

---
+++ src/utils/const.jsx
@@ -0,0 +1,32 @@
+export const headerMenu = [
+   {
+      path: "/",
+      title: "Home",
+
+   },
+   {
+      path: "/tv-shows",
+      title: "TV Shows",
+
+   },
+   {
+      path: "/movies",
+      title: "Movies",
+
+   },
+   {
+      path: "/latest",
+      title: "New & Popular",
+
+   },
+   {
+      path: "/my-list",
+      title: "My List",
+
+   },
+   {
+      path: "/original-audio",
+      title: "Browse by Languages",
+
+   }
+]
