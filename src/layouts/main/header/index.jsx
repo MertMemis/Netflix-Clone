0d95adbd9cb0e0ce2924550e7e10494b5efb4633
MertMemis <mertmemis66@gmail.com>
2024-01-09 19:56:13 +0300

    Update header

--- src/layouts/main/header/index.jsx
+++ src/layouts/main/header/index.jsx
@@ -8,11 +8,11 @@ import SearchBar from "./search-bar";
 
 export default function Header() {
    return(
-      <header className="w-full bg-black min-h-[70px] flex sticky top-0">
+      <header className="w-full bg-black min-h-[70px] flex sticky top-0 z-[20]">
          <div className="flex items-center sticky mx-[60px] w-full">
             <NetflixLogo />
-            <nav className=" flex item-center">    
-               {headerMenu.map((menu, index) => (
+            <nav className=" flex item-center invisible md:visible">    
+               {headerMenu.map((menu) => (
                   <NavLink to={menu.path} >
                      {({isActive}) => (
                         <div 
