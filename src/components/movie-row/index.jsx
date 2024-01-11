39b1c3baa03a3c38f242447df5fbe038fe29e698
MertMemis <mertmemis66@gmail.com>
2024-01-11 20:43:39 +0300

    Movie Row

--- src/components/movie-row/index.jsx
+++ src/components/movie-row/index.jsx
@@ -5,3 +5,30 @@ const base_url = "https://image.tmdb.org/t/p/original/";
 
 export default function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.table(movies);
+  return (
+    <div className="">
+      <h2>{title}</h2>
+
+      <div className="flex">
+        {movies.map((movie) => (
+          <img
+            src={`${base_url}${movie.backdrop_path}`}
+            alt={movie.name}
+            className="w-full object-contain max-h-[100px] mr-[10px] transition duration-450 hover:scale-105"
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
