6b35a9c679b5ffc5a43f77ceb01bbfe26e1dbdff
MertMemis <mertmemis66@gmail.com>
2024-01-11 20:45:16 +0300

    Movie İmages

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,17 @@
-import Row from '~/components/row';
-import requests from '~/utils/api/requests';
-
+import Row from "~/components/movie-row/index.jsx";
+import requests from "~/utils/api/requests";
 
 export default function Home() {
-   return(
-      <div>
-         <Row title="Popular on Netflix" fetchUrl={requests.fetchPopularNetflix}/>
-         <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-      </div>
-   )
+  return (
+    <div>
+      <Row title="Popular on Netflix" fetchUrl={requests.fetchPopularNetflix} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rating" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documantaries" fetchUrl={requests.fetchDocumantaries} />
+    </div>
+  );
 }
