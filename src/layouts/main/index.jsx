8499f3bbbd992b9421523ab492cbf7c77a252cc6
MertMemis <mertmemis66@gmail.com>
2024-01-12 19:12:35 +0300

    Update index.jsx

--- src/layouts/main/index.jsx
+++ src/layouts/main/index.jsx
@@ -2,10 +2,10 @@ import { Outlet } from "react-router-dom";
 import Header from "./header";
 
 export default function MainLayout() {
-   return (
-      <div>
-         <Header />
-         <Outlet/>
-      </div>
-   )
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
