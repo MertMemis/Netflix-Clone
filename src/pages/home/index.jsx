a8dca3c66ad7a3718cf79284950d3273b1d2632b
MertMemis <mertmemis66@gmail.com>
2024-01-20 22:03:08 +0300

    update home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,30 +2,63 @@ import Banner from "~/components/banner";
 import Row from "~/components/movie-row/index.jsx";
 import requests from "~/utils/api/requests";
 import Header from "~/components/header";
+import { WindowVirtualizer } from "virtua";
 
 export default function Home() {
   return (
-    <div>
-      <div className="relative">
-        <Header />
-        <Banner />
+    <div className="relative">
+      <Header />
+      <Banner />
 
-        <div className=" relative z-10">
-          <Row
-            title="Popular on Netflix"
-            fetchUrl={requests.fetchPopularNetflix}
-          />
-          <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-          <Row title="Top Rating" fetchUrl={requests.fetchTopRated} />
-          <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-          <Row title="Animations" fetchUrl={requests.fetchAnimations} />
-          <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-          <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-          <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-          <Row title="Family" fetchUrl={requests.fetchFamily} />
-          <Row title="Crime" fetchUrl={requests.fetchCrime} />
-        </div>
-      </div>
+      <WindowVirtualizer startMargin={1}>
+        <Row
+          title="Popular on Netflix"
+          fetchUrl={requests.fetchPopularNetflix}
+        />
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rating" fetchUrl={requests.fetchTopRated} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Animations" fetchUrl={requests.fetchAnimations} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Family" fetchUrl={requests.fetchFamily} />
+        <Row title="Crime" fetchUrl={requests.fetchCrime} />
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rating" fetchUrl={requests.fetchTopRated} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Animations" fetchUrl={requests.fetchAnimations} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Family" fetchUrl={requests.fetchFamily} />
+        <Row title="Crime" fetchUrl={requests.fetchCrime} />
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rating" fetchUrl={requests.fetchTopRated} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Animations" fetchUrl={requests.fetchAnimations} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Family" fetchUrl={requests.fetchFamily} />
+        <Row title="Crime" fetchUrl={requests.fetchCrime} />
+      </WindowVirtualizer>
     </div>
   );
+}
+
+{
+  /* <Row
+          title="Popular on Netflix"
+          fetchUrl={requests.fetchPopularNetflix}
+        />
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rating" fetchUrl={requests.fetchTopRated} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Animations" fetchUrl={requests.fetchAnimations} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Family" fetchUrl={requests.fetchFamily} />
+        <Row title="Crime" fetchUrl={requests.fetchCrime} /> */
 }
