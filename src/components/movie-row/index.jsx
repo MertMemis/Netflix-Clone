e70f7a4f9df118a1c45e402ed8d327531d8dadb7
MertMemis <mertmemis66@gmail.com>
2024-01-20 22:02:54 +0300

    update row

--- src/components/movie-row/index.jsx
+++ src/components/movie-row/index.jsx
@@ -7,6 +7,7 @@ const base_url = "https://image.tmdb.org/t/p/original/";
 export default function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
   const [translateValue, setTranslateValue] = useState(0);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -37,10 +38,10 @@ export default function Row({ title, fetchUrl }) {
   };
 
   return (
-    <div className="pl-[70px] overflow-hidden relative group">
+    <div className="pl-[70px] overflow-hidden">
       <h2 className="text-2xl mt-8 mb-3">{title}</h2>
 
-      <div className="absolute z-10 flex items-center w-full h-[130px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+      <div className="absolute z-10 flex w-full h-[130px]">
         <button
           className={`-ml-[70px] bg-[#14141480] hover:bg-[#141414b3] h-full w-16 ${
             translateValue === 0 ? "invisible" : "visible"
@@ -53,12 +54,12 @@ export default function Row({ title, fetchUrl }) {
           className="ml-auto pr-32 w-24 h-full bg-[#14141480] hover:bg-[#141414b3] text-center"
           onClick={() => handleScroll("right")}
         >
-          <IoIosArrowForward size={40} />
+          <IoIosArrowForward size={40} className=" ml-4" />
         </button>
       </div>
 
       <div
-        className="flex transition-transform duration-700 ease-in-out"
+        className="flex transition-transform duration-1000 ease-in-out"
         style={{ transform: `translateX(${translateValue}px)` }}
       >
         {movies.map((movie, index) => (
