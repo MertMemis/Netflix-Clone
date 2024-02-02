baec774ac204bc0dc53b742f1bfc49370bf39c27
MertMemis <mertmemis66@gmail.com>
2024-02-02 15:32:11 +0300

    button visibility

--- src/components/movie-row/index.jsx
+++ src/components/movie-row/index.jsx
@@ -7,7 +7,6 @@ const base_url = "https://image.tmdb.org/t/p/original/";
 export default function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
   const [translateValue, setTranslateValue] = useState(0);
-  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -20,7 +19,6 @@ export default function Row({ title, fetchUrl }) {
   }, [fetchUrl]);
 
   const handleScroll = (direction) => {
-    const container = document.getElementById("movie-container");
     const cardWidth = 1423;
     const maxTranslateValue = (movies.length - 35) * cardWidth;
 
@@ -38,14 +36,12 @@ export default function Row({ title, fetchUrl }) {
   };
 
   return (
-    <div className="pl-[70px] overflow-hidden">
+    <div className="pl-[70px] overflow-hidden relative group">
       <h2 className="text-2xl mt-8 mb-3">{title}</h2>
 
-      <div className="absolute z-10 flex w-full h-[130px]">
+      <div className="absolute z-10 flex w-full h-[130px] opacity-0 group-hover:opacity-100 transition-opacity duration-300">
         <button
-          className={`-ml-[70px] bg-[#14141480] hover:bg-[#141414b3] h-full w-16 ${
-            translateValue === 0 ? "invisible" : "visible"
-          }`}
+          className="-ml-[70px] bg-[#14141480] hover:bg-[#141414b3] h-full w-16"
           onClick={() => handleScroll("left")}
         >
           <IoIosArrowBack size={40} />
@@ -59,7 +55,7 @@ export default function Row({ title, fetchUrl }) {
       </div>
 
       <div
-        className="flex transition-transform duration-1000 ease-in-out"
+        className="flex transition-transform duration-1000 ease-in-out group"
         style={{ transform: `translateX(${translateValue}px)` }}
       >
         {movies.map((movie, index) => (
