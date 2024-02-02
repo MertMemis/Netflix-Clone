c70117ac815ac71afcc81426f9a0b99c67b8ce4a
MertMemis <mertmemis66@gmail.com>
2024-02-02 17:48:45 +0300

    Update const.jsx

--- src/utils/const.jsx
+++ src/utils/const.jsx
@@ -1,32 +1,26 @@
 export const headerMenu = [
-   {
-      path: "/",
-      title: "Home",
-
-   },
-   {
-      path: "/tv-shows",
-      title: "TV Shows",
-
-   },
-   {
-      path: "/movies",
-      title: "Movies",
-
-   },
-   {
-      path: "/latest",
-      title: "New & Popular",
-
-   },
-   {
-      path: "/my-list",
-      title: "My List",
-
-   },
-   {
-      path: "/original-audio",
-      title: "Browse by Languages",
-
-   }
-]
+  {
+    path: "/",
+    title: "Home",
+  },
+  {
+    path: "/tv-shows",
+    title: "TV Shows",
+  },
+  {
+    path: "/movies",
+    title: "Movies",
+  },
+  {
+    path: "/latest",
+    title: "New & Popular",
+  },
+  {
+    path: "/my-list",
+    title: "My List",
+  },
+  {
+    path: "/original-audio",
+    title: "Browse by Languages",
+  },
+];
